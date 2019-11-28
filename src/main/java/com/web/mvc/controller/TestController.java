b6fef44e4d82f22391eebecaa9518a6230be21f6
MB-Teacher <MB-Teacher@MB-207-teacher>
2019-11-28 20:57:41 +0800

    SpringMVC

---
+++ src/main/java/com/web/mvc/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.web.mvc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+
+public class TestController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest hsr, HttpServletResponse hsr1) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
