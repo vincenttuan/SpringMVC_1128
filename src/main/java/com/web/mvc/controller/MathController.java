f1d7636224ea64edbd1f98b8391b0b171d4b9957
MB-Teacher <MB-Teacher@MB-207-teacher>
2019-12-03 21:01:47 +0800

    Java

--- src/main/java/com/web/mvc/controller/MathController.java
+++ src/main/java/com/web/mvc/controller/MathController.java
@@ -4,14 +4,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/math")
 public class MathController {
+    
     @RequestMapping("/add/{x}/{y}")
     public String add(@PathVariable Integer x, @PathVariable Integer y, Model model) {
         Integer sum = x + y;
         model.addAttribute("sum", sum);
         return "math_result";
     }
+    
+    @RequestMapping("/sub")
+    public String sub(@RequestParam(name = "x", defaultValue = "0", required = false) Integer x,
+                      @RequestParam(name = "y", defaultValue = "0", required = false) Integer y,
+                      Model model) {
+        Integer sum = x - y;
+        model.addAttribute("sum", sum);
+        return "math_result";
+    }
+    
 }
