19a0d39b1dc9244132f8f8f0edf423b4c1f34e8c
MB-Teacher <MB-Teacher@MB-207-teacher>
2019-12-05 19:50:46 +0800

    Java

--- src/main/java/com/web/mvc/controller/LottoController.java
+++ src/main/java/com/web/mvc/controller/LottoController.java
@@ -1,12 +1,15 @@
 package com.web.mvc.controller;
 
 import com.google.gson.Gson;
+import com.web.mvc.service.LottoService;
+import com.web.mvc.service.LottoServiceImpl;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 import java.util.stream.IntStream;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,17 +19,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/api/lotto")
 public class LottoController {
     
+    @Autowired
+    private LottoService lottoService;
+    
     @RequestMapping("/539")
     @ResponseBody
     public String lotto539(@RequestParam(name = "count", defaultValue = "1", required = false) Integer count) {
-        Map<Integer, Set<Integer>> lottos = new LinkedHashMap<>();
-        IntStream.rangeClosed(1, count).forEach(i -> {
-            Set<Integer> lotto = new LinkedHashSet<>();
-            while(lotto.size() < 5) {
-                lotto.add(new Random().nextInt(39)+1);
-            }
-            lottos.put(i, lotto);
-        });
-        return new Gson().toJson(lottos);
+        return new Gson().toJson(lottoService.lotto539(count));
+    }
+    
+    @RequestMapping("/4star")
+    @ResponseBody
+    public String lotto4star(@RequestParam(name = "count", defaultValue = "1", required = false) Integer count) {
+        return new Gson().toJson(lottoService.lotto4Star(count));
+    }
+    
+    @RequestMapping("/3star")
+    @ResponseBody
+    public String lotto3star(@RequestParam(name = "count", defaultValue = "1", required = false) Integer count) {
+        return new Gson().toJson(lottoService.lotto3Star(count));
     }
 }
