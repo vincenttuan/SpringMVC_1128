e1d448f79860a19cd0ea17f616050531739e7436
MB-Teacher <MB-Teacher@MB-207-teacher>
2019-12-05 21:37:59 +0800

    Java

--- src/main/java/com/web/mvc/controller/WeatherController.java
+++ src/main/java/com/web/mvc/controller/WeatherController.java
@@ -1,21 +1,11 @@
 package com.web.mvc.controller;
 
-import com.google.gson.Gson;
-import com.web.mvc.service.LottoService;
-import com.web.mvc.service.LottoServiceImpl;
 import com.web.mvc.service.WeatherService;
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
-import java.util.Map;
-import java.util.Random;
-import java.util.Set;
-import java.util.stream.IntStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/api")
