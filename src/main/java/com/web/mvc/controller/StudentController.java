491ff50b5a1080275cbc66dcb2ed168f7fb1327e
MB-Teacher <MB-Teacher@MB-207-teacher>
2019-12-10 20:59:58 +0800

    Java

--- src/main/java/com/web/mvc/controller/StudentController.java
+++ src/main/java/com/web/mvc/controller/StudentController.java
@@ -14,40 +14,47 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/student")
 public class StudentController {
+
     static List<Student> students;
+
     static {
         students = new CopyOnWriteArrayList<>();
         students.add(new Student("Vincent", "boy"));
         students.add(new Student("Anita", "girl"));
     }
-    
+
     @RequestMapping(value = "/input")
     public String input(Model model) {
         model.addAttribute("student", new Student());
         model.addAttribute("action", "add");
         return "student";
     }
-    
+
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     public String add(Student student, Model model) {
         students.add(student);
         model.addAttribute("students", students);
         return "student_list";
     }
-    
+
     @RequestMapping(value = "/get/{name}", method = RequestMethod.GET)
     public String get(@PathVariable("name") String name, Model model) {
         Optional<Student> student = students.stream()
                 .filter(s -> s.getName().equals(name))
                 .findAny();
-        model.addAttribute("student", student.isPresent()?student.get():new Student());
+        model.addAttribute("student", student.isPresent() ? student.get() : new Student());
         model.addAttribute("action", "update");
         return "student";
     }
-    
+
     @RequestMapping(value = "/update", method = RequestMethod.POST)
-    @ResponseBody
     public String update(Student student, Model model) {
-        return "update";
+        students.stream()
+                .filter(s -> s.getName().equals(student.getName()))
+                .forEach(s -> {
+                    s.setSex(student.getSex());
+                });
+        model.addAttribute("students", students);
+        return "student_list";
     }
 }
