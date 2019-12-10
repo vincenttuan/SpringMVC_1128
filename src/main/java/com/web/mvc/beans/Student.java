d80be6601b8fa47aaac14d64890d1502103ed1a0
MB-Teacher <MB-Teacher@MB-207-teacher>
2019-12-10 18:55:55 +0800

    Java

---
+++ src/main/java/com/web/mvc/beans/Student.java
@@ -0,0 +1,23 @@
+package com.web.mvc.beans;
+
+public class Student {
+    private String name;
+    private String sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+    
+}
